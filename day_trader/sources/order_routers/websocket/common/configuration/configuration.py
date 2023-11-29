4eefdc44ba2eef48d17f1968ed7caa809cf64dd6
FerGusMos <fernando.mosca@tradehelm.com>
2023-11-29 10:14:04 -0300

    SecondsToSleepOnTradeForMock missing param @Websocket module

--- day_trader/sources/order_routers/websocket/common/configuration/configuration.py
+++ day_trader/sources/order_routers/websocket/common/configuration/configuration.py
@@ -10,6 +10,6 @@ class Configuration:
         self.Mode = config['DEFAULT']['MODE']
 
         self.ImplementMock=config['DEFAULT']['IMPLEMENT_MOCK']=="True"
-
+        self.SecondsToSleepOnTradeForMock = float(config['DEFAULT']['SECONDS_TO_SLEEP_ON_TRADE_FOR_MOCK'])
         self.WaitForConnectionsPacingSec = int(config['DEFAULT']['WAIT_FOR_CONNECTIONS_PACING_SEC'])
 
